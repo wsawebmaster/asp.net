cba850e2b16580ce44bf24b67848a33b679f300d
Wagner Andrade <wsawebmaster@yahoo.com.br>
2026-05-12 21:51:08 -0300

    feat: adiciona Models Produto com método ListarProdutos

---
+++ ProjetoMVC/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjetoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        // Método que retorna uma lista de produtos
+        public static List<Produto> ListarProdutos()
+        {
+            return new List<Produto>
+            {
+                new Produto { Id = 1, Nome = "Notebook", Preco = 3500.00 },
+                new Produto { Id = 2, Nome = "Smartphone", Preco = 100.00 }
+            };
+        }
+    }
+}
