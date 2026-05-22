386330ff5ac7cfd91b5869cc4b680f3a4baa6f02
Wagner Andrade <wsawebmaster@yahoo.com.br>
2026-05-21 22:07:28 -0300

    feat(auth): adiciona Projeto06.Identity com autenticação e autorização via ASP.NET Core Identity

---
+++ Projeto06.Identity/Data/Migrations/20260522005847_CriarTabelasIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto06.Identity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelasIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
